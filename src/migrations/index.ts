0780e4cd35e2d223f26a1e8617e463b628c38604
Brian Robinson <brian.robinson@deptagency.com>
2026-04-05 15:59:05 +0100

    feat: add initial database migration for production Supabase setup

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260405_145855 from './20260405_145855';
+
+export const migrations = [
+  {
+    up: migration_20260405_145855.up,
+    down: migration_20260405_145855.down,
+    name: '20260405_145855'
+  },
+];
